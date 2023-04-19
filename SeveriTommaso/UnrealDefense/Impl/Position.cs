59ec67c3ed40f813e3f976d936eecf531ddaaf9d
Tommaso Severi <tommaso.severi2@studio.unibo.it>
2023-04-19 19:11:56 +0200

    Small addition from the last commit

---
+++ SeveriTommaso/UnrealDefense/Impl/Position.cs
@@ -0,0 +1,10 @@
+public class Position : IPosition
+{
+    public double X { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
+    public double Y { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
+
+    public Position(double x, double y)
+    {
+        throw new System.NotImplementedException();
+    }
+}
