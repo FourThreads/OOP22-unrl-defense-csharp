d1a5594fef31d1bbf176ae588867783629d4f2c3
Danilo <magliadanilo@gmail.com>
2023-04-19 09:53:44 +0200

    Added common classes

--- MagliaDanilo/UnrealDefense/Common/Pair.cs
+++ MagliaDanilo/UnrealDefense/Common/Pair.cs
@@ -1,7 +1,19 @@
 namespace MagliaDanilo.UnrealDefense.Common
 {
-    public class Pair
+    public class Pair<TOne, TTwo>
     {
-        
+        private TOne FirstElement { get; set; }
+        private TTwo SecondElement { get; set; }
+
+        public Pair(TOne firstElement, TTwo secondElement)
+        {
+            FirstElement = firstElement;
+            SecondElement = secondElement;
+        }
+
+        public Pair<TOne, TTwo> Copy()
+        {
+            return new Pair<TOne, TTwo>(FirstElement, SecondElement);
+        }
     }
 }
