8a435313b8401d56fc22105d0bafa29d83442b94
Francesco Buda <francesco.buda3@studio.unibo.it>
2023-04-20 11:49:43 +0200

    filled Bank and Integrity interfaces

--- BudaFrancesco/UnrealDefense/Api/IBank.cs
+++ BudaFrancesco/UnrealDefense/Api/IBank.cs
@@ -2,6 +2,8 @@ namespace BudaFrancesco.UnrealDefense.Api
 {
     public interface IBank
     {
-        
+        double Money { get; }
+        void AddMoney(double money);
+        Boolean TrySpend(double price);
     }
 }
--- BudaFrancesco/UnrealDefense/Api/IIntegrity.cs
+++ BudaFrancesco/UnrealDefense/Api/IIntegrity.cs
@@ -2,6 +2,8 @@ namespace BudaFrancesco.UnrealDefense.Api
 {
     public interface IIntegrity
     {
-        
+        int Hearts { get; }
+        void Damage(int hearts);
+        Boolean IsCompromised();
     }
 }
