2c6dc2bab90132901dc8b819ff89bf6cb867f56d
TommasoCere <cere.cere_2001@libero.it>
2023-04-20 16:00:46 +0200

    impl Tower and ITower

--- CerediTommaso/UnrealDefense/Api/ITower.cs
+++ CerediTommaso/UnrealDefense/Api/ITower.cs
@@ -1,6 +1,29 @@
 using System;
 
-namespace it.unibo.unrldef.model.api
+namespace CerediTommaso.UnrealDefense.Api
 {
-    
+    /// <summary>
+    /// A tower that can be placed in a world.
+    /// </summary>
+    /// <author> tommaso.ceredi@studio.unibo.it </author>
+    public interface ITower : IEntity
+    {
+        /// <summary>
+        /// The cost of the tower.
+        /// </summary>
+        /// <returns> the cost of the tower </returns>
+        int Cost { get; }
+        
+        /// <summary>
+        /// Create a copy of the tower.
+        /// </summary>
+        Tower Copy();
+        
+        void override SetParentWorld(World world);
+        
+        /// <summary>
+        /// The enemy that the tower is attacking, if any.
+        /// </summary>
+        Enemy Target { get; }
+    }
 }
---
+++ CerediTommaso/UnrealDefense/Impl/Tower.cs
@@ -0,0 +1,58 @@
+﻿System.Collections.Generic;
+
+namespace CerediTommaso.UnrealDefense.Impl
+{
+    /// <summary>
+    /// A tower that can be placed in a world.
+    /// </summary>
+    /// <author> tommaso.ceredi@studio.unibo.it </author>
+    public abstract class Tower : DefenseEntity, ITower
+    {
+        private readonly int cost;
+        private Enemy? target;
+        
+        /// <summary>
+        /// Constructor of the tower.
+        /// </summary>
+        /// <param name="name"> the name of the tower </param>
+        /// <param name="radius"> the radius of the tower </param>
+        /// <param name="damage"> the damage of the tower </param>
+        /// <param name="attackRate"> the attack rate of the tower </param>
+        /// <param name="cost"> the cost of the tower </param>
+        public Tower(string name, int radius, int damage, int attackRate, int cost) : base(name, radius, damage, attackRate)
+        {
+            this.cost = cost;
+        }
+        
+        public abstract Tower Copy();
+        
+        public int Cost => this.cost;
+
+        public void UpdateState(long time)
+        {
+            this.IncrementTime(time);
+            this.CheckAttack();
+        }
+
+        protected override void Attack()
+        {
+            List<Enemy> enemiesInRange = this.ParentWorld.SorroundingEnemies(this.Position.Value, this.Radius);
+            if (enemiesInRange.Count > 0)
+            {
+                if this.target == null || !enemiesInRange.Contains(this.target.Value)
+                {
+                    this.target = enemiesInRange[0];
+                }
+                this.target.Value.ReduceHealth(this.Damage);
+                this.AdditionalAttack(this.target.Value);
+            } else
+            {
+                this.target = null;
+            }
+        }
+        
+        public Enemy? Target => this.target;
+        
+        protected abstract void AdditionalAttack(Enemy enemy);
+    }
+}
